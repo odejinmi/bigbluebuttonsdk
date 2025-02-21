f59634c79472e9f7fd4edf622c6d07b155892fcc
videx <45696129+odejinmi@users.noreply.github.com>
2025-02-21 21:50:28 +0100

    dependency updated

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,12 +6,14 @@ import FlutterMacOS
 import Foundation
 
 import bigbluebuttonsdk
+import file_picker
 import flutter_webrtc
 import path_provider_foundation
 import speech_to_text
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   BigbluebuttonsdkPlugin.register(with: registry.registrar(forPlugin: "BigbluebuttonsdkPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FlutterWebRTCPlugin.register(with: registry.registrar(forPlugin: "FlutterWebRTCPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
